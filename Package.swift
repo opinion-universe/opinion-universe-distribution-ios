387c6dd7f920473686539f066a618d91aac25b17
Opinion <anand@opinionuniverse.com>
2026-02-10 18:31:07 +0530

    channel and payout type added in response and in request added payout type

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
         .binaryTarget(
             name: "OpinionUniverseSDK",
             // This URL must point to the ZIP you upload to your GitHub Release
-            url: "https://github.com/opinion-universe/opinion-universe-distribution-ios/releases/download/2.0.0/OpinionUniverseSDK.xcframework.zip",
-            checksum: "bdb748ad9413df72265addea2e217cb1be74aeea2bff743eb4835d935c081d08"
+            url: "https://github.com/opinion-universe/opinion-universe-distribution-ios/releases/download/2.0.1/OpinionUniverseSDK.xcframework.zip",
+            checksum: "93962f722b993fe59781d41d737ebe6cb650bc4fd5ec84ae41d6a59e7df62629"
         ),
     ]
 )
